95e6bcb96c3875dfcd03de98632bc71ed6167d2c
Satoru Yonekura <mail2yonekura@gmail.com>
2014-06-17 03:39:45 +0900

    add random button, use lib

--- ui/Property Controller.jsx
+++ ui/Property Controller.jsx
@@ -7,14 +7,29 @@
 (function(thisObj) {
   
   //========================================
+  // Load Module
+  //========================================
+  var aesu = {};
+  
+  (function() {
+    #include ../lib/math.jsxinc
+  }).call(aesu);
+  
+  (function() {
+    #include ../lib/property.jsxinc
+  }).call(aesu);
+  
+  
+  //========================================
   // Constants
   //========================================
-  var binary_icon_set = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x1E\x00\x00\x00\x14\b\x06\x00\x00\x00\u009A\u00AB\u008D\u00C4\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x01#IDATx\u00DA\u00ECU\x0B\r\u00C20\x10]\t\x02&a8\u00A8\u0084\u00E1\x00\x1C\u00E0\u0080\u00A0`A\x01\x12\u0086\x03p\x00\x0E\u00C0\x018\x18\nV\u00DE\u00C1\x1B\u00B947\u00BE\x01\x12B\u0093\u0097u\u00BD\u00EB{\u00BD\u00F6\u00AEu!\u0084\u00E4\x1B\u00AD\u0093|\u00A9\u00FD\u0085?\u00D6\u00BA\u00D6 \x12.\u00C3'\u00E3\u00EF\u00C19\u00B7}\u00AB0\x04S|\x16\x14\u00DD+S\u00FF^Bp\u00CC\u00B8\u00D8\u00E9#\x117\u0093z/\x04\u00E3\u0081\u00F5\u00A3g\x1CGjF\x04T\u00E1\u00DCJ\u00EER3\u00BE\u00A2\u00F0H\u00FA\u00C4\u00A8\u008D\u00E8\u0082\u00BA\u00AE\x07@\x00J \u00D36\u00DA\x0B`\x03\u00A4\u00C4B|i\u00F3@N{\u00C9~n\u00F1\u009C.-\u0083\\\u009CW\u00D6\x02\u00D0\u00DF\u00C9\u00E2\"\u00DF*\u009A/s\x0BK\u00EC\u00AA\u00B0\"\u00F0$\u00A9$:\u008E\x05\x0B\u00CF\b\u00B7\u00D61Kh\b\u00A4<\u00B7K\u0086\u00BB\u00A8\u00BD|\u00814\u0089\x12eh\u00A2\x12n\t\u008C\u00EF\u00E0\u00CDn98\u00FD:1+uf\u008B\u00F0\x04A\u00CD\u00D5\u00C5\u00D2\u00D4\u00F9V\u00EDN_q\f\u00E8#\u00F6\u0083\u0094\u0096U\u00D3.~\x16\u00F1\u009F+\u00C2uK%x\x1E\u0081\u00E9\u00C3\u009D\u00F3\u00D78\u00DC\u00FF=\u00FEy\u00E1\u00A3\x00\x03\x00\u00D4f\u008B\u00D4\u00DC\u00BDW\u00FE\x00\x00\x00\x00IEND\u00AEB`\u0082";
-  var binary_icon_add = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x14\x00\x00\x00\x14\b\x06\x00\x00\x00\u008D\u0089\x1D\r\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x00bIDATx\u00DAb\u00FC\u00FF\u00FF?\x035\x01\u00E3\u00A8\u0081\u0083\u00CF@&B\n\u0080\x16:\x00q=\u00D1&\u0082\\\u0088\x0F\u00FF\u00FB\u00F7\u00AF\x1E\u0088\u00FF\x13R\x07\u00C3L\fT\x06T7\x10\u00977\u00FF\x13\u00C0\u00F5\u00B8\u00BC\u008C5\u0096A\x11\u0081\u00C4\u008D\x07\u00E2\x04 vD\x12{\u00C0\u00C8\u00C8\u00F8`\u00F8F\u00CAC >0\u009A\u0097\u0087\u00B0\u0081\x00\x01\x06\x00\u00CB\x0E\u00F0\u00E5\u00D1\x00_2\x00\x00\x00\x00IEND\u00AEB`\u0082";
-  var binary_icon_sub = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x14\x00\x00\x00\x14\b\x06\x00\x00\x00\u008D\u0089\x1D\r\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x00AIDATx\u00DAb\u00FC\u00FF\u00FF?\x035\x01\x13\x03\u0095\u00C1\u00A8\u0081\u00A3\x06\x0Ej\x03\u00819\u00A8\u00FE?&\u00D8O\u00C8@F\\Y\x0F(\u00AE\x00\u00A4\x14\u00D0\u0084?022^ \u00CB\u00C0\u00D1X\x1E5\u0090\u008E\x06\x02\x04\x18\x00\u00F6T%E\u009B\u00D1>?\x00\x00\x00\x00IEND\u00AEB`\u0082";
-  var binary_icon_mul = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x14\x00\x00\x00\x14\b\x06\x00\x00\x00\u008D\u0089\x1D\r\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x00\u009AIDATx\u00DAb\u00FC\u00FF\u00FF?\x035\x01\u00E3\u00A8\u0081\u0083\u00CF@&\\\x12@\u008B\x04\u0088\x11#\u00CA@\u00A0F\x05 u\x1FH'\u00A0\x19\u00B6\x1FH\u00D7\u00E35\x11\u00E4el\u00F8\u00DF\u00BF\x7F\u00F3\u0081\u00F8?\x10'\x00\u00B1\x00\x10\u009F\u0087b\x01\\z\u00C0\u00C1\u0087O\x12\u00C9\u00D0\u00FB\u00C4\x18F\u008C\u0081 \u0097\u00BD\u0087\x1AZ@\u00C80\u00BC\x06\u00A2y\x13\u00EE}B\x06\u00B2\u00E0\u0089\u00E1\u00FDP\u00AE###\u00E3\x07h\u00F2\u009A\x0F\u00A2\u0081\u00FC\x05\u00B8\u00E2\u0084\x05O|]\x00\u00E2B\u0090a\u00E0\x04\u00CB\u00C8\u0098\b4\u00EC\x03T|4\u00EB\u008Dh\x03\x01\x02\f\x00\u00DE\u0082HP3\x00A*\x00\x00\x00\x00IEND\u00AEB`\u0082";
-  var binary_icon_div = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x14\x00\x00\x00\x14\b\x06\x00\x00\x00\u008D\u0089\x1D\r\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x00yIDATx\u00DAb\u00FC\u00FF\u00FF?\x035\x01\x13\x03\u0095\u00C1\u00A8\u0081t0\x10\u0098\n\u00FA\u0081x=\x10\x0B\x10e\"(\u00D9\u00E0\u00C3\u00FF\u00FE\u00FD\u00FB\x0F\u00C5\x0E\u0084\u00D4\u00820\x0B\x11v\x06\x02\u00B1\x02##\u00E3\x01b\x1C\u00C8H\u0097\u0084\u00FD\u009F0p\u00C0e ./;\x12p\u00C8\x05\u00B2\u00BD\f\u0094W\x00R\x02\u00C00\u00BC@\u00ADX~\x0F\u008De\x03bb\u0099\u0098\u0084\u00FD\x00\u0088?@\u00F1 \u0089\u00E5Q\x03I\x02\x00\x01\x06\x00\x18\u00F3\u00BER[\u00A5\u00C7\u0099\x00\x00\x00\x00IEND\u00AEB`\u0082";
-
+  var binary_icon_set = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x1E\x00\x00\x00\x14\b\x06\x00\x00\x00\u009A\u00AB\u008D\u00C4\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x01+IDATx\u00DA\u00ECU\u0081\r\u00820\x10\x04\u00E3\x022\x02\u008EPG\u0090\x11t\x04W\u00C0\x11`\x04\x19\x01F\u0080\x11\u00EC\b:\u0082\u008C\u0080\u00F7\u00C9\u00D5T\x04\u0081\u0088\u0092\x18\u009A|\u00DA\u00D2\u00FF\u00BF\u00FF\u00BFoq\u00AB\u00AAr\u00A6\x18\x0Bg\u00A21\x03\u00FFl,\u009B>j\u00AD}L>\u00B7\u00A5RJ\x7F\x15\x18\u0080+L)A\u00AF\u00D6Q\u00D0\u00D7!|D\f6\x1E\u0092\u00B11Z\x7F\u0090\u008C\u0082\x14CK-\u0099\u00EA\x1E\x19\x1D R\u009D\x04rD\u00A0%\u00BF+\u008A\u008F\u00FD\u0096&\x19\u00CE\u0093\u00AE\u00E6\x12\u0085\x10F'\u00F2\\\x07\r1\u0089C\u00A9\u0088G\u00F0\u00C8\x00@bRTp\x1D\u00B7e\u00EF\u00D6_.Fj\x00$\u0090\x18\x11_yva\u0086\u0099\u00A5\u009Bb\u00EFY\u00F6\u00B9\u0080uq\u00FCr\u009D` F\u00D2L\x1B\u0096\u00FE\u00CC\u00A63T\u00A4\u00D8W\"X\u00E7\u00CCz\u009C\u00EB\u00C4\x00\u00E0[\u00EF\u00B1\u00BC\u00D5\x1A&\u0090\u00E0F}@\u00AC\u00CC\u00EC\x0Eu\u00AC\u00AB\u0095\u00B1\u00B1\u00BA\u0086\u00DF\u00A5\u00F0\u00C41\u00F9\u00B1\u00EF\u00B0\"\u00A7\u0089\u00F5\u00B0\u00A4\u00F5\u00EE'5\u00C6\u00C7\u008E:r^\u00B6\u00F1\u00DD\u00D6\\\x0F\u00BE[\u00AE\u00942\u00DC6\u00E9\u00B0r\u00EA\u009D\x0Fw\u00FE\x1F\u00FF=\u00F0]\u0080\x01\x005Q\u008BDz\u00DC\u00BB\u0083\x00\x00\x00\x00IEND\u00AEB`\u0082";
+  var binary_icon_add = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x14\x00\x00\x00\x14\b\x06\x00\x00\x00\u008D\u0089\x1D\r\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x00dIDATx\u00DAb\u00FC\u00FF\u00FF?\x035\x01\x13\x03\u0095\u00C1\u00A8\u0081t0\u00F0\u00EC\u00D9\u00B3i@\u00BC\u009B\u009A.\x14\x04b\u0097\x01\u00F32\x0B\x0Eo\"\u00BBH\t\u008B\u00D8=cc\u00E3{D\x1B\b\x04\u00BB\t\u0088U\x00q'V\u009D\u00A0\u00AC\u0087\x0F\u009F9s\u00A6\x1C\u0088\u00FF\x13R\x07\u00C3\u00C3#a\u009F\u0085\u0086\x19Q\u0080q\u00B4\u00F8\x1A|\x06\x02\x04\x18\x00\b\u00B0S\u00F8\u00B5\u00DD\u00BBS\x00\x00\x00\x00IEND\u00AEB`\u0082";
+  var binary_icon_sub = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x14\x00\x00\x00\x14\b\x06\x00\x00\x00\u008D\u0089\x1D\r\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x00FIDATx\u00DAb\u00FC\u00FF\u00FF?\x035\x01\x13\x03\u0095\u00C1\u00A8\u0081\u00A3\x06\u0092\x01XpI\u009C={V\tH)\u00A1\t\u00BF766>K\u0096\u0081@\x10\n\u00C4\x1Dhb{\u0080\u00D8\x15\u009F\u0081\u008C\u00A3Yo\u00D4\u00C0\u00E1h @\u0080\x01\x00:\u00E0\r\x03\u00B7\u008BPZ\x00\x00\x00\x00IEND\u00AEB`\u0082";
+  var binary_icon_mul = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x14\x00\x00\x00\x14\b\x06\x00\x00\x00\u008D\u0089\x1D\r\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x00\u008EIDATx\u00DAb\u00FC\u00FF\u00FF?\x035\x01\u00E3\u00A8\u0081\u0083\u00CF@\x06\u0090\u0081\u00D8\u00F0\u00993g\\\u00B0\u0088\x19\x03\u00B1 .= \u00CC\u0084\u00CD\u0092\u00B3g\u00CF*\x01\u00A9\u00DD@z&\u0092\u00981H\f\u0088\u00D3\u00C8\u00F22\u00D0\x00\u0090F\u0090\u0081\u00B3\u00A0\x18d\u00D8jcc\u00E3t\u00B2\u00C3\x10\u00C9P\x10\u0098E\u00C80\x10`\" \x7F\x16\u0088\u00DFS%\u0096\u0091\u00C2l5\u00D4\u00E0\u0099\u00C4\u00B8\x12\u00AB\u0081\u00D0H9\u0083\x1CfH\u00DE\u00AF\x00\u008Au\u0092\u0093l\u00D2\u00B0\u0088\u0085\x12J6\u00A3yy$\x18\b\x10`\x005\u00A6\u00CD\u00E6v^\u00AA\\\x00\x00\x00\x00IEND\u00AEB`\u0082";
+  var binary_icon_div = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x14\x00\x00\x00\x14\b\x06\x00\x00\x00\u008D\u0089\x1D\r\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x00yIDATx\u00DAb\u00FC\u00FF\u00FF?\x035\x01\x13\x03\u0095\u00C1\u00A8\u0081\u0094\x03\x16B\n\u00CE\u009E={\x06H)\x01\u00B1\u0089\u00B1\u00B1\u00F1=j\u00B8\x10d\u0098 \x14S\u00EEB\u0090\u00CB@\u0086\x01]w\u0096\x18\x03\x19\u00B1%l\u00A07]\b\u0085\x04\u00D0\u0082\u00F7\u00A4\u00B8p7\x01\x03]\u0081x\x0F\u00D1.\u00A4i\u00B2\x01z?\x14\u0088\u00CB\u00A9\u0099\x0EW\x01q\x07\x11\u00E1Jt,wB\u0093\x0E\u00F9\u00B1<Z8\f\u00AC\u0081\x00\x01\x06\x00H\u00CE$\u00BB\x04<_2\x00\x00\x00\x00IEND\u00AEB`\u0082";
+  var binary_icon_rnd = "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x1E\x00\x00\x00\x14\b\x06\x00\x00\x00\u009A\u00AB\u008D\u00C4\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\u00C9e<\x00\x00\x00\u00FDIDATx\u00DA\u00ECU\u008B\t\u00830\x10M\u008A\x0B\u00D8\x11\\!+\u00B8\u0082\u008E`G\u00B0#\u00D4\x11\u00DA\x11\u00EA\bu\x04\u00B3\u0082#\u00D4\x11\u00EC;x\u0082\u0084\u00C6Xi)\u0088\x07GB\u00EE\u00B8\u0097{/\x1F=\f\u0083\u00FA\u0087\x1D\u00D4\u009Fl\x07\u00DE\x06\u00B0\u00B5\u00F6\x02/\u00DE\u00C5\"'1\u00C5\u00F0\u0098.\u00C1O\u00C6\x18\u00BB\x12\u00DB\u00C0\u00FB\u00C5\x1D\x03H\u008B\x13\u00F8\u00FE\x0B6\u00A2@\u00BC\u0086\x17S\u00EA0tpa&\u00E3<\x1F\x19A\\:,\x19S\u00EC\u00B6\u00F9Hc\x14\u0089\t\u00DA8\u00D4]e\u008D\u008Ct\u00E3\u00C6\u0098/\u00ECX\u00C6\u008E\u008C/?\\(\"\u00CF\u00D9\x13.\u00C5r'\\\u00A1\u00F0\u008Ds\u00D9T\u00C2yF\u0099*\u008E\u00BDO\u00DFY\u008D1\u009C\u00D9\u00A1k\u00BEb\u00F1\\\u0087\u008B\u00A9\u00E6\u00CE\u00FB\u00A9\u00C6+,^{\u008F\x05\u00BC\u00A4~!\x13\u00DAS\u00E4&\u0094\u00AB\u00F40\x16\x06\u00A6\u0096\u008B\u00BA\u00E6\u00C9\u0096\u00FC\x16\u00A0-\u00B5\u00AF}\u00F9\u00FA\u00DB\u00DF\"\u00AF\u0094\n=:z\u00FF\u008F7\x0F\u00FC\x12`\x00\x14\u00B6Z\u0099\u00BC\u00F6\u00C8\x0B\x00\x00\x00\x00IEND\u00AEB`\u0082";
+  
   var PATH_DEPTH = 6, 
       PATH_NUM_ROW = Math.ceil(PATH_DEPTH / 2);
 
@@ -24,18 +39,6 @@
   //========================================
   // Utilities
   //----------------------------------------
-  var rgbToHex = function(r, g, b) {
-    var rgb = b | (g << 8) | (r << 16);
-    return rgb //(0x1000000 | rgb).toString(16).slice(1);
-  };
-
-  var hexToRGB = function(hexColor) {
-    var r = hexColor >> 16;
-    var g = (hexColor & 0x00ff00) >> 8;
-    var b = hexColor & 0xff;
-    return [r, g, b];
-  };
-
   var isNumber = function(obj) {
     if(typeof(obj) != "number" && typeof(obj) != "string")
       return false;
@@ -63,69 +66,7 @@
       callback(layers[i], comp);
     }
   };
-
-  var setPropertyValue = function(property, newValue, propertyValueType) {
-    if(property.propertyType === PropertyType.PROPERTY && 
-       property.propertyValueType === propertyValueType) 
-    {
-      if(property.canVaryOverTime && property.numKeys > 0) {
-        var layer = property;
-        while(layer.containingComp === undefined) layer = layer.parentProperty;
-        property.setValueAtTime(layer.time, newValue);
-      } else {
-        property.setValue(newValue);
-      }
-    }
-  };
-
-  var shiftPropertyValue = function(property, newValue, propertyValueType) {
-    if(property.propertyType === PropertyType.PROPERTY && 
-       property.propertyValueType === propertyValueType) 
-    {
-      if(property.canVaryOverTime && property.numKeys > 0) {
-        var propValue = property.value, 
-            keyValue, diff;
-        
-        if(property.propertyValueType === PropertyValueType.ThreeD_SPATIAL || 
-           property.propertyValueType === PropertyValueType.ThreeD)
-        {
-          diff = [newValue[0] - propValue[0], 
-                  newValue[1] - propValue[1], 
-                  newValue[2] - propValue[2]];
-          
-          for(var k = 1; k <= property.numKeys; k++) {
-            keyValue = property.keyValue(k);
-            property.setValueAtKey(k, [keyValue[0] + diff[0], 
-                                       keyValue[1] + diff[1], 
-                                       keyValue[2] + diff[2]]);
-          }
-        }
-         
-        if(property.propertyValueType === PropertyValueType.TwoD_SPATIAL || 
-           property.propertyValueType === PropertyValueType.TwoD)
-        {
-          diff = [newValue[0] - propValue[0], 
-                  newValue[1] - propValue[1]];
-          
-          for(var k = 1; k <= property.numKeys; k++) {
-            keyValue = property.keyValue(k);
-            property.setValueAtKey(k, [keyValue[0] + diff[0], 
-                                       keyValue[1] + diff[1]]);
-          }
-        }
-       
-        if(property.propertyValueType === PropertyValueType.OneD) {
-          diff = newValue - property.value;
-          
-          for(var k = 1; k <= property.numKeys; k++)
-            property.setValueAtKey(k, property.keyValue(k) + diff);
-        }
-      } else {
-        property.setValue(newValue);
-      }
-    }
-  };
-
+  
 
   //----------------------------------------
   // Number Array Operator
@@ -168,6 +109,25 @@
     }
     return res;
   }
+  
+  var opRnd = function(oldValue, newValue) {
+    var res = [], rnd1, rnd2, rnd;
+
+    for(var i = 0; i < oldValue.length; i++) {
+      if(newValue[i] !== null) {
+        rnd1 = Math.sqrt(-2 * Math.log(Math.random()));
+        rnd2 = 2 * Math.PI * Math.random();
+        rnd = Math.random() > 0.5? rnd = rnd1 * Math.cos(rnd2) : rnd = rnd1 * Math.sin(rnd2);;
+        rnd = rnd * newValue[i];
+        
+        res[i] = oldValue[i] + rnd;
+      } else {
+        res[i] = oldValue[i];
+      }
+    }
+    
+    return res;
+  }
 
   var calc = function(oldValue, newValue, op) {
     var res;
@@ -186,6 +146,9 @@
         break;
       case "div":
         res = opDiv(oldValue, newValue);
+        break;
+      case "rnd":
+        res = opRnd(oldValue, newValue);
         break;
     }
     
@@ -236,18 +199,17 @@
     }
     
     if(shift)
-      shiftPropertyValue(prop, result, prop.propertyValueType);
+      aesu.shiftPropertyValue(prop, result);
     else
-      setPropertyValue(prop, result, prop.propertyValueType);
-    
+      aesu.setPropertyValue(prop, result);
   };
 
   var applyLayerProperty = function(prop, newValue) {
-    setPropertyValue(prop, newValue, PropertyValueType.LAYER_INDEX);
+    aesu.setPropertyValue(prop, newValue);
   };
 
   var applyColorProperty = function(prop, newValue) {
-    setPropertyValue(prop, newValue, PropertyValueType.COLOR);
+    aesu.setPropertyValue(prop, newValue);
   };
 
 
@@ -267,7 +229,7 @@
 
   var grp_float_row2, grp_float_row4,
       cb_float_increment, cb_float_shift, 
-      btn_float_set, btn_float_add, btn_float_sub, btn_float_mul, btn_float_div;
+      btn_float_set, btn_float_add, btn_float_sub, btn_float_mul, btn_float_div, btn_float_rnd;
       
   var ddl_layer_list;
 
@@ -456,6 +418,7 @@
     btn_float_sub.value = (this === btn_float_sub)? true : false;
     btn_float_mul.value = (this === btn_float_mul)? true : false;
     btn_float_div.value = (this === btn_float_div)? true : false;
+    btn_float_rnd.value = (this === btn_float_rnd)? true : false;
     
     selectedOperator = this.properties.name;
   };
@@ -505,11 +468,11 @@
   };
 
   var btnColorOnClick = function() {
-    var defaultColor = rgbToHex(pickedColor[0] * 255, pickedColor[1] * 255, pickedColor[2] * 255);
+    var defaultColor = aesu.rgbToHex(pickedColor[0] * 255, pickedColor[1] * 255, pickedColor[2] * 255);
     var hexColor = $.colorPicker(defaultColor);
     
     if(hexColor !== -1) {
-      var rgb = hexToRGB(hexColor);
+      var rgb = aesu.hexToRGB(hexColor);
       pickedColor = [rgb[0]/255, rgb[1]/255, rgb[2]/255, 1.0];
     }
   };
@@ -759,6 +722,10 @@
     btn_float_div = grp_float_row4.add("iconButton", undefined, binary_icon_div, {toggle: true, name: "div"});
     btn_float_div.preferredSize = btnSize;
     btn_float_div.onClick = btnFloatOperatorOnClick;
+    
+    btn_float_rnd = grp_float_row4.add("iconButton", undefined, binary_icon_rnd, {toggle: true, name: "rnd"});
+    btn_float_rnd.preferredSize = [32, 22];
+    btn_float_rnd.onClick = btnFloatOperatorOnClick;
     
     
     //---------- number end
