fc8ab34d649b48cb39f6fdc2e19b0a23a9463a7c
Satoru Yonekura <mail2yonekura@hotmail.com>
2017-10-30 03:16:07 +0900

    make use matchname

--- alongMotionPath.jsx
+++ alongMotionPath.jsx
@@ -12,8 +12,8 @@
   // Expression
   //----------------------------------------
   var EXP_POSITION = """res = value;
-L = null; try{L = effect("Rail Layer")(1);}catch(e){}
-offset = 0; try{offset = L.effect("Offset")(1);}catch(e){}
+L = null; try{L = effect("Motion Path Layer")(1);}catch(e){}
+offset = 0; try{offset = L.effect("Offset All")(1);}catch(e){}
 if(L !== null){
   inP = L.inPoint; outP = L.outPoint;
   offset = (offset + effect("Offset")(1)) % 100;
@@ -23,8 +23,8 @@ if(L !== null){
 res;""";
 
   var EXP_POSITION_2D3D = """res = value;
-L = null; try{L = effect("Rail Layer")(1);}catch(e){}
-offset = 0; try{offset = L.effect("Offset")(1);}catch(e){}
+L = null; try{L = effect("Motion Path Layer")(1);}catch(e){}
+offset = 0; try{offset = L.effect("Offset All")(1);}catch(e){}
 if(L !== null){
   inP = L.inPoint; outP = L.outPoint;
   offset = (offset + effect("Offset")(1)) % 100;
@@ -35,11 +35,11 @@ res;""";
 
   var EXP_ORIENTATION = """res = value;
 current = toWorld(anchorPoint);
-L = null; try{L = effect("Rail Layer")(1);}catch(e){}
-offset = 0; try{offset = L.effect("Offset")(1);}catch(e){}
+L = null; try{L = effect("Motion Path Layer")(1);}catch(e){}
+offset = 0; try{offset = L.effect("Offset All")(1);}catch(e){}
 if(L !== null){
   inP = L.inPoint; outP = L.outPoint;
-  offset = (offset + effect("Offset")(1) + 0.01) % 100;
+  offset = (offset + effect("Offset")(1)) % 100;
   t = linear(offset, 0, 100, inP, outP);
   next = L.toWorld(L.anchorPoint, t);
   res = lookAt(current, next);
@@ -48,27 +48,27 @@ res;""";
 
   var EXP_ROTATION = """res = value;
 current = toWorld(anchorPoint);
-L = null; try{L = effect("Rail Layer")(1);}catch(e){}
-offset = 0; try{offset = L.effect("Offset")(1);}catch(e){}
+L = null; try{L = effect("Motion Path Layer")(1);}catch(e){}
+offset = 0; try{offset = L.effect("Offset All")(1);}catch(e){}
 if(L !== null){
   inP = L.inPoint; outP = L.outPoint;
-  offset = (offset + effect("Offset")(1) + 0.01) % 100;
+  offset = (offset + effect("Offset")(1)) % 100;
   t = linear(offset, 0, 100, inP, outP);
-  len = L.toWorld(L.anchorPoint, t) - current;
-  res += radiansToDegrees(Math.atan2(len[1], len[0]));;
+  len = L.toWorld(L.anchorPoint, t + 0.001) - current;
+  res += radiansToDegrees(Math.atan2(len[1], len[0]));
 }
 res;""";
 
   var EXP_ROTATION_2D3D = """res = value;
 current = toWorld(anchorPoint);
-L = null; try{L = effect("Rail Layer")(1);}catch(e){}
-offset = 0; try{offset = L.effect("Offset")(1);}catch(e){}
+L = null; try{L = effect("Motion Path Layer")(1);}catch(e){}
+offset = 0; try{offset = L.effect("Offset All")(1);}catch(e){}
 if(L !== null){
   inP = L.inPoint; outP = L.outPoint;
-  offset = (offset + effect("Offset")(1) + 0.01) % 100;
+  offset = (offset + effect("Offset")(1)) % 100;
   t = linear(offset, 0, 100, inP, outP);
-  len = L.toComp(L.anchorPoint, t) - current;
-  res += radiansToDegrees(Math.atan2(len[1], len[0]));;
+  len = L.toComp(L.anchorPoint, t + 0.001) - current;
+  res += radiansToDegrees(Math.atan2(len[1], len[0]));
 }
 res;""";
 
@@ -81,9 +81,9 @@ res;""";
   };
 
   var addExpressionController = function(layer, name, value, matchName) {
-    if(layer.property("Effects").property(name) !== null) return;
+    if(layer.property("ADBE Effect Parade").property(name) !== null) return;
     
-    var ctrl = layer.property("Effects").addProperty(matchName);
+    var ctrl = layer.property("ADBE Effect Parade").addProperty(matchName);
     ctrl.name = name;
     ctrl.property(1).setValue(value);
   };
@@ -102,21 +102,21 @@ res;""";
       
     switch(dimCheck) {
       case "3D3D":
-        layer.position.expression = EXP_POSITION;
-        layer.orientation.expression = EXP_ORIENTATION;
+        layer.property("ADBE Transform Group").property("ADBE Position").expression = EXP_POSITION;
+        layer.property("ADBE Transform Group").property("ADBE Orientation").expression = EXP_ORIENTATION;
         break;
       case "2D2D":
-        layer.position.expression = EXP_POSITION;
-        layer.rotation.expression = EXP_ROTATION;
+        layer.property("ADBE Transform Group").property("ADBE Position").expression = EXP_POSITION;
+        layer.property("ADBE Transform Group").property("ADBE Rotate Z").expression = EXP_ROTATION;
         break;
       case "2D3D":
-        layer.position.expression = EXP_POSITION_2D3D;
-        layer.rotation.expression = EXP_ROTATION_2D3D;
+        layer.property("ADBE Transform Group").property("ADBE Position").expression = EXP_POSITION_2D3D;
+        layer.property("ADBE Transform Group").property("ADBE Rotate Z").expression = EXP_ROTATION_2D3D;
         break;
     }
     
     if(dimCheck !== null) {
-      addExpressionController(layer, "Rail Layer", refLayer.index, "ADBE Layer Control");
+      addExpressionController(layer, "Motion Path Layer", refLayer.index, "ADBE Layer Control");
       addExpressionController(layer, "Offset", 0, "ADBE Slider Control");
     }
   };
@@ -128,13 +128,13 @@ res;""";
   if(!(comp instanceof CompItem)) return;
   if(comp.selectedLayers.length < 2) return;
   
-  app.beginUndoGroup("Rail Motion Path");
+  app.beginUndoGroup("Along Motion Path");
   
   var selLayers = comp.selectedLayers.slice(0, -1), 
       refLayer = comp.selectedLayers[comp.selectedLayers.length - 1];
   
   if(!instanceOfAVLayer(refLayer)) return;  
-  addExpressionController(refLayer, "Offset", 0, "ADBE Slider Control");
+  addExpressionController(refLayer, "Offset All", 0, "ADBE Slider Control");
   
   for(var i = 0; i < selLayers.length; i++)
     setupLayer(selLayers[i], refLayer);
